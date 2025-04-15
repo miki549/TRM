8b09d42af9a5efab9e0c3e9bbf2718c02dfe4109
Miklós Szücs <szucs549@gmail.com>
2025-04-15 22:46:45 +0200

    refaktorálva

--- compare/TRM_external_flows.m
+++ compare/TRM_external_flows.m
@@ -13,7 +13,7 @@ function out = TRM_external_flows(t, N)
 
     % Itt definiálhatók a különböző időfüggő áramlások
     % Példák kommentben:
-    flow(1, 1) = 0.05 * (sin(t/50) + 1.5);  % Első útszakaszra változó beáramlás
+    %flow(1, 1) = 0.05 * (sin(t/50) + 1.5);  % Első útszakaszra változó beáramlás
     %flow(2, 1) = morning_peak + evening_peak; % Második útszakaszra csúcsidőszaki beáramlás
     %flow(3, 2) = 0.03 * (cos(t/70) + 1.2);   % Harmadik útszakaszról változó kiáramlás
 
